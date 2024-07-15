048d535e102dead6cd2fdd6f5d68208418ef91a9
Jon Pretty <jon.pretty@propensive.com>
2024-07-15 09:23:30 +0200

    Simplification and add a home page

--- src/web/blog.invariant.Main.scala
+++ src/web/blog.invariant.Main.scala
@@ -21,12 +21,6 @@ import htmlRenderers.scalaSyntax
 given Realm = realm"invariant"
 given Message is Loggable = safely(supervise(Log.route(Out))).or(Log.silent)
 
-given InitError is Fatal = error =>
-  Out.println(error.stackTrace.teletype)
-  ExitStatus.Fail(1)
-
-case class InitError(detail: Message) extends Error(detail)
-
 def menu = Map
  (t"Home"    -> % / p"",
   t"About"   -> % / p"about",
@@ -50,11 +44,7 @@ def page(side: Seq[Html[Flow]], content: Html[Flow]*): HtmlDoc =
 @main
 def server(): Unit =
   erased given ConcurrencyError is Unchecked = ###
-  quash:
-    case InitError(message) =>
-      Out.println(message)
-      ExitStatus.Fail(1).terminate()
-  .within(supervise(tcp"8080".serve[Http](handle)))
+  supervise(tcp"8080".serve[Http](handle))
 
 class Service() extends JavaServlet(handle)
 
@@ -63,6 +53,24 @@ def notFound(path: Text): HtmlDoc = page
   H1(t"Not Found"),
   P(t"There is no page at the url ", Code(path), t"."))
 def about: HtmlDoc = page(Nil, H1(t"About"), P(t"Invariant.blog is a blog about invariance."))
+
+def home: HtmlDoc =
+  val markdown = md"""
+Welcome to my blog. Here I write about programming, primarily with
+[Scala 3](https://scala-lang.org/), but more generally in advanced statically-typed programming
+languages. This is my outlet to elaborate on the technical decisions, influences and motivations that
+contributed towards my main software projects: [Soundness](https://soundness.dev/),
+[Fury](https://github.com/propensive/fury), [Amok](https://github.com/propensive/amok) and
+[CoDL](https://github.com/propensive/codl).
+
+This blog is new, so there is only one post so far; the first in a series about error handling.
+  """
+
+  page
+   (Nil,
+    H2(t"A blog about programming with ", A(href = url"https://soundness.dev/")(t"Soundness")),
+    Div
+     (markdown.html*))
 
 def contact: HtmlDoc =
   page(Nil, H1(t"Contact Me"), P(t"To get in touch, please email me at jon.pretty@propensive.com"))
@@ -91,6 +99,7 @@ def handle(using HttpRequest): HttpResponse[?] =
 
   .within:
     request.path match
+      case %                           => HttpResponse(home)
       case % / p"about"                => HttpResponse(about)
       case % / p"contact"              => HttpResponse(contact)
       case % / p"images" / Name(image) => HttpResponse(Classpath / p"images" / Name(image))
